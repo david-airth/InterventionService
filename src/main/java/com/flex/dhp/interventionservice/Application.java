60988c2c9e07b7ebb95395a6390e79601977fa3c
David Airth <david.airth@slalom.com>
2017-07-19 13:11:22 -0700

    First commit

---
+++ src/main/java/com/flex/dhp/interventionservice/Application.java
@@ -0,0 +1,60 @@
+package com.flex.dhp.interventionservice;
+
+import com.flex.dhp.common.model.*;
+import com.flex.dhp.common.repository.AssessmentRepository;
+import com.flex.dhp.common.repository.CareplanRepository;
+import com.flex.dhp.common.repository.InterventionRepository;
+import com.flex.dhp.common.repository.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan(basePackages = "com.flex.dhp")
+@EnableAutoConfiguration
+@EnableJpaRepositories(basePackages = "com.flex.dhp")
+@EntityScan(basePackages = "com.flex.dhp")
+@SpringBootApplication(scanBasePackages = "com.flex.dhp")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(PatientRepository patientRepository,
+                           CareplanRepository careplanRepository,
+                           AssessmentRepository assessmentRepository,
+                           InterventionRepository interventionRepository) {
+        return (evt) -> Arrays.asList(
+                "Joe:Smith,Jan:Jones,Bob:Adams,Janet:Lee,Richard:Rho,Don:Fisher,Shirley:Pollack,Susan:Long".split(","))
+                .forEach(
+                        a -> {
+                            String name[] = a.split(":");
+                            Patient patient = patientRepository.save(new Patient(name[0], name[1]));
+
+                            Careplan careplan1 = new Careplan(patient, "Care Plan 1");
+                            Careplan careplan2 = new Careplan(patient, "Care Plan 2");
+                            careplanRepository.save(careplan1);
+                            careplanRepository.save(careplan2);
+
+                            assessmentRepository.save(new Assessment(careplan1, "Assessment 1"));
+                            assessmentRepository.save(new Assessment(careplan1, "Assessment 2"));
+                            assessmentRepository.save(new Assessment(careplan2, "Assessment 1"));
+                            assessmentRepository.save(new Assessment(careplan2, "Assessment 2"));
+
+                            interventionRepository.save(new Intervention(careplan1, InterventionType.Medication, "Intervention 1"));
+                            interventionRepository.save(new Intervention(careplan1, InterventionType.Medication, "Intervention 2"));
+                            interventionRepository.save(new Intervention(careplan2, InterventionType.Medication, "Intervention 1"));
+                            interventionRepository.save(new Intervention(careplan2, InterventionType.Medication, "Intervention 2"));
+                        });
+    }
+}
---
+++ src/test/java/com/flex/dhp/interventionservice/AbstractRestControllerTests.java
@@ -0,0 +1,105 @@
+package com.flex.dhp.interventionservice;
+
+import com.flex.dhp.common.model.*;
+import com.flex.dhp.common.repository.AssessmentRepository;
+import com.flex.dhp.common.repository.CareplanRepository;
+import com.flex.dhp.common.repository.InterventionRepository;
+import com.flex.dhp.common.repository.PatientRepository;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * Created by david.airth on 7/11/17.
+ */
+public abstract class AbstractRestControllerTests {
+    protected MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    protected MockMvc mockMvc;
+
+    protected String firstName = "Jane";
+    protected String lastName = "Doe";
+
+    protected HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    protected Patient patient;
+
+    protected List<Careplan> careplanList = new ArrayList<>();
+    protected List<Assessment> assessmentList = new ArrayList<>();
+    protected List<Intervention> interventionList = new ArrayList<>();
+
+    @Autowired
+    protected InterventionRepository interventionRepository;
+
+    @Autowired
+    protected AssessmentRepository assessmentRepository;
+
+    @Autowired
+    protected PatientRepository patientRepository;
+
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+
+    @Autowired
+    protected CareplanRepository cardcareRepository;
+
+    @Autowired
+    protected void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+        this.interventionRepository.deleteAllInBatch();
+        this.assessmentRepository.deleteAllInBatch();
+        this.cardcareRepository.deleteAllInBatch();
+        this.patientRepository.deleteAllInBatch();
+
+        this.patient = patientRepository.save(new Patient(firstName, lastName));
+
+        this.careplanList.add(cardcareRepository.save(new Careplan(patient, "Care Plan 1")));
+        this.careplanList.add(cardcareRepository.save(new Careplan(patient, "Care Plan 2")));
+
+        this.assessmentList.add(assessmentRepository.save(new Assessment(this.careplanList.get(0), "Assessment 1")));
+        this.assessmentList.add(assessmentRepository.save(new Assessment(this.careplanList.get(0), "Assessment 2")));
+        this.assessmentList.add(assessmentRepository.save(new Assessment(this.careplanList.get(1), "Assessment 1")));
+        this.assessmentList.add(assessmentRepository.save(new Assessment(this.careplanList.get(1), "Assessment 2")));
+
+        this.interventionList.add(interventionRepository.save(new Intervention(this.careplanList.get(0), InterventionType.Medication, "Intervention 1")));
+        this.interventionList.add(interventionRepository.save(new Intervention(this.careplanList.get(0), InterventionType.Medication, "Intervention 2")));
+        this.interventionList.add(interventionRepository.save(new Intervention(this.careplanList.get(1), InterventionType.Medication, "Intervention 1")));
+        this.interventionList.add(interventionRepository.save(new Intervention(this.careplanList.get(1), InterventionType.Medication, "Intervention 2")));
+    }
+
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
+}
